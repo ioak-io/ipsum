486a395c330fe8b344650f48892b8a2767e970f6
Arun Kumar Selvaraj <dev@ioak.org>
2020-04-05 20:49:13 +0530

    Theme and styling for the app

--- src/components/App/__tests__/App.spec.ts
+++ src/components/App/__tests__/App.spec.ts
@@ -1,10 +1,35 @@
-import { shallowMount } from '@vue/test-utils';
+import { shallowMount, createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
 import App from '@/components/App/App.vue';
 
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+localVue.use(Vuex);
+const router = new VueRouter();
+
 describe('App.vue', () => {
+  let getters;
+  let store: any;
+
+  beforeEach(() => {
+    getters = {
+      getProfile: () => {
+        return {
+          space: 'teststace',
+        };
+      },
+    };
+    store = new Vuex.Store({
+      getters,
+    });
+  });
   it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(App, {
+      store,
+      localVue,
+      router,
       propsData: { msg },
     });
     expect(wrapper.contains('#app')).toBeTruthy();
