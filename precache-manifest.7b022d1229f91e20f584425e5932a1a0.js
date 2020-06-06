b63753eb6c074d2dc7a8fc01911bb8c25ae9598f
Arun Kumar Selvaraj <dev@ioak.org>
2020-06-06 10:02:12 +0530

    deploy

---
+++ precache-manifest.7b022d1229f91e20f584425e5932a1a0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ab4af7703884331e38",
+    "url": "/css/app.a552f7ec.css"
+  },
+  {
+    "revision": "ef2a85b9a7c2da29204e9345b618d0c1",
+    "url": "/img/background.ef2a85b9.jpg"
+  },
+  {
+    "revision": "c6bbae5cd10af682608da25594647df3",
+    "url": "/img/background2.c6bbae5c.jpg"
+  },
+  {
+    "revision": "287b8ed1d8e2478058ab412576360006",
+    "url": "/img/ipsum_black.287b8ed1.svg"
+  },
+  {
+    "revision": "7c74b4564bde7cd40efac6758a02daff",
+    "url": "/img/ipsum_white.7c74b456.svg"
+  },
+  {
+    "revision": "d8493743920775aaa10aff3e39016d2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ab4af7703884331e38",
+    "url": "/js/app.1f9d0cfa.js"
+  },
+  {
+    "revision": "e669c222487f0046bec6",
+    "url": "/js/chunk-vendors.85dce4df.js"
+  },
+  {
+    "revision": "8851c4d6d5caec6c0c0d6265792a8caf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
