6fa46fa73f13faeddd7e5e07048f7543d0a19bed
Arunkumar Selvaraj <arun@ioak.io>
2023-01-04 17:36:40 +0530

    redux store integration

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../../store/store';
 import Content from './Content';
 import './style.scss';
 
 const App = (props: any) => {
   return (
-    <div className="app">
+    <Provider store={store}>
         <Content />
-    </div>
+    </Provider>
   );
 };
 
